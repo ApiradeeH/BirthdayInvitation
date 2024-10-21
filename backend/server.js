f26de8fa7672cbd482e397567a052acbd68d5b92
ApiradeeH <apiradeehaeusler@gmail.com>
2024-10-21 14:34:59 +0200

    updated

--- backend/server.js
+++ backend/server.js
@@ -28,9 +28,9 @@ app.use("/api/", guestRoute);
 
 // !! Your middleware should not go below this line !!
 // Serve frontend client/build folder
-app.use(express.static(path.join(__dirname, "client/dist")));
+app.use(express.static(path.join(__dirname, "../client/dist")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+  res.sendFile(path.join(__dirname + "../client/dist/index.html"));
 });
 
 // MongoDB connection
