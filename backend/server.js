3de97510446ba389471277d196b6ac223d3da3df
ApiradeeH <apiradeehaeusler@gmail.com>
2024-10-21 13:13:12 +0200

    cors origin changed

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ dotenv.config();
 
 app.use(
   cors({
-    origin: "http://localhost:5173", // Replace with your frontend URL
+    origin: "https://birthdayinvitation.onrender.com/", // Replace with your frontend URL
     methods: ["GET", "POST", "DELETE"],
     credentials: true,
   })
